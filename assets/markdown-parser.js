7dc13be2a3b36efdab6a5f8e09cdb9426dffaf73
liuliming <liulimingwhuthit@gmail.com>
2025-06-03 13:54:46 +0800

    修改md转换器

--- assets/markdown-parser.js
+++ assets/markdown-parser.js
@@ -2,12 +2,12 @@
 function parseMarkdown(markdown) {
     // 替换规则
     const rules = [
-        { regex: /^## (.*$)/gim, replace: '<h3>$1</h3>' },
-        { regex: /^### (.*$)/gim, replace: '<h4>$1</h4>' },
+        { regex: /## (.*$)/gim, replace: '<h3>$1</h3>' },
+        { regex: /### (.*$)/gim, replace: '<h4>$1</h4>' },
         { regex: /\*\*(.*?)\*\*/g, replace: '<strong>$1</strong>' },
         { regex: /\*(.*?)\*/g, replace: '<em>$1</em>' },
         { regex: /`(.*?)`/g, replace: '<code>$1</code>' },
-        { regex: /^> (.*$)/gim, replace: '<blockquote>$1</blockquote>' },
+        { regex: /> (.*$)/gim, replace: '<blockquote>$1</blockquote>' },
         { regex: /\[(.*?)\]\((.*?)\)/g, replace: '<a href="$2">$1</a>' },
         { regex: /\n\n/g, replace: '</p><p>' },  // 双换行转换为段落
         { regex: /\n/g, replace: '<br>' }        // 单换行转换为换行符
